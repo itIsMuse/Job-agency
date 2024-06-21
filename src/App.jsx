4c78faaf19ef765d45dc7fae8080ab445802b080
itIsMuse <oyeniranshock@gmail.comgit config --global user.email oyeniranshock@gmail.com>
2024-06-21 21:52:54 +0100

    restatrted new method(active recall), restarted from joblist, realised that i understood some concepts i thought i didnt understand

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import HomeCards from "./Components/HomeCards";
-// import logo from "./assets/images/logo.png";
 import JobListings from "./Components/jobListings";
 const App = () => {
   return (
--- src/Components/jobListings.jsx
+++ src/Components/jobListings.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import jobs from "../jobs.json";
-import Jobscards from "./Jobs-for-cards";
+import JobListing from "./JobListing";
 
 const JobListings = () => {
-  return <Jobscards />;
+  const [fullDescripton, setFullDescription] = useState(false);
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {jobs.map((job) => {
+            return <JobListing key={job.id} job={job} />;
+          })}
+          <button>full</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default JobListings;
