5ee9d1424746d21326bdc4972e88660d958530ae
itIsMuse <oyeniranshock@gmail.comgit config --global user.email oyeniranshock@gmail.com>
2024-07-13 01:29:12 +0100

    code dididnt dtill work ooo

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { ClipLoader } from "react-spinners/ClipLoader";
+import { FadeLoader } from "react-spinners/FadeLoader";
 
 const Spinner = ({ loading }) => {
   const override = {
+    display: "block",
     margin: "100px auto",
   };
   return (
-    <>
-      <ClipLoader
-        color="#1591ea"
-        loading={loading}
-        cssOverride={override}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-    </>
+    <FadeLoader
+      color="#1591ea"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    />
   );
 };
 
