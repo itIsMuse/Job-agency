e66d8bbf7f784aadc30358d21df23af538e23c96
itIsMuse <oyeniranshock@gmail.comgit config --global user.email oyeniranshock@gmail.com>
2024-07-29 22:01:40 +0100

    couldnt do much, need to start adding time to prep in terms of coding

--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const JobPage = () => {
+  useEffect(() => {
+    const url = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+    const fetchJobs = async () => {
+      try {
+        const result = await fetch(url);
+        const data = await result.json();
+        setJob(data);
+      } catch (error) {
+        console.log("error loading", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
   return <div>JobPage</div>;
 };
 
