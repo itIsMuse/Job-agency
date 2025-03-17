80a1fea06f47c35ae3fa0881b22e355616de273d
itIsMuse <oyeniranshock@gmail.com>
2025-03-17 10:55:06 +0100

    chore: Refactor JobPage component and improve delete functionality

--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -1,130 +1,107 @@
 import React from "react";
 import { useParams, useLoaderData, useNavigate } from "react-router-dom";
-import { FaArrowLeft, FaMapMarker} from "react-icons/fa";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 
+const jobLoader = async ({ params }) => {
+  const result = await fetch(`/api/jobs/${params.id}`);
+  const data = await result.json();
+  return data;
+};
 
-const jobLoader = async({params}) => {
-  const result = await fetch (`/api/jobs/${params.id}`)
-    const data = await  result.json()
-    return data
-  }
+const JobPage = ({ deleteJob }) => {
+  const { id } = useParams();
+  const job = useLoaderData();
+  const navigate = useNavigate();
 
+  const onDelete = async (id) => {
+    const confirm = window.confirm("Are you sure?");
+    if (!confirm) return;
 
-const JobPage = ({deleteJob}) =>{
-  const {id} = useParams()
-  const job = useLoaderData()
-  const navigate = useNavigate()
-// console.log (job)
-const [jobList, setJoblist] = useState(job)
+    await deleteJob(id); // Ensure job is deleted before proceeding
+    toast.success("Job successfully removed");
 
-const onDelete = (id) => {
-const confirm = window.confirm('Are you sure ?');
+    // Navigate after state updates
+    setTimeout(() => navigate("/jobs"), 1000);
+  };
 
-if (!confirm) {
-return   
-}
-deleteJob(id)
-
-toast.success('Job successfully removed')
-
-return navigate('/jobs')
-}
-
-return (
-  <>
-        <section>
-      <div className="container m-auto py-6 px-6">
-        <Link
-          to ="/jobs"
-          className="text-indigo-500 hover:text-indigo-600 flex items-center">
-        
-         <FaArrowLeft/> Back to Job Listings</Link>
-      </div>
-    </section>
-
-    <section className="bg-indigo-50">
-      <div className="container m-auto py-10 px-6">
-        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-          <main>
-            <div
-              className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
-            >
-              <div className="text-gray-500 mb-4">{job.type}</div>
-              <h1 className="text-3xl font-bold mb-4">
-                {job.title}
-              </h1>
-              <div
-                className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
-              >
-                <FaMapMarker/>
-                <p className="text-orange-700">{job.location}</p>
-              </div>
-            </div>
-
-            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-              <h3 className="text-indigo-800 text-lg font-bold mb-6">
-                {job.title}
-              </h3>
-
-              <p className="mb-4">
-              {job.description}
-              </p>
-
-              <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
-
-              <p className="mb-4">{job.salary}</p>
-            </div>
-          </main>
-
-          
-          <aside>
-
-            <div className="bg-white p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-bold mb-6">Company Info</h3>
-
-              <h2 className="text-2xl">{job.company.name}</h2>
-
-              <p className="my-2">
-                {job.company.description}
-              </p>
-
-              <hr className="my-4" />
-
-              <h3 className="text-xl">Contact Email:</h3>
-
-              <p className="my-2 bg-indigo-100 p-2 font-bold">
-                {job.company.contactEmail}
-              </p>
-
-              <h3 className="text-xl">Contact Phone:</h3>
-
-              <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
-            </div>
-
-            
-            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-              <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-              <Link
-                to = {`/jobs/edit/${job.id}`}
-                className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job </Link
-              >
-              <button onClick={() => {onDelete(job.id)}}
-                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-              >
-                Delete Job
-              </button>
-            </div>
-          </aside>
+  return (
+    <>
+      <ToastContainer />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            to="/jobs"
+            className="text-indigo-500 hover:text-indigo-600 flex items-center"
+          >
+            <FaArrowLeft /> Back to Job Listings
+          </Link>
         </div>
-      </div>
-    </section>
-  </>
-)
-}
+      </section>
 
+      <section className="bg-indigo-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <main>
+              <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+                <div className="text-gray-500 mb-4">{job.type}</div>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
+                <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                  <FaMapMarker />
+                  <p className="text-orange-700">{job.location}</p>
+                </div>
+              </div>
 
-export { JobPage as default, jobLoader}
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-indigo-800 text-lg font-bold mb-6">
+                  {job.title}
+                </h3>
+                <p className="mb-4">{job.description}</p>
+                <h3 className="text-indigo-800 text-lg font-bold mb-2">
+                  Salary
+                </h3>
+                <p className="mb-4">{job.salary}</p>
+              </div>
+            </main>
+
+            <aside>
+              <div className="bg-white p-6 rounded-lg shadow-md">
+                <h3 className="text-xl font-bold mb-6">Company Info</h3>
+                <h2 className="text-2xl">{job.name}</h2>
+                <p className="my-2">{job.description}</p>
+                <hr className="my-4" />
+                <h3 className="text-xl">Contact Email:</h3>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.contactEmail}
+                </p>
+                <h3 className="text-xl">Contact Phone:</h3>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.contactPhone}
+                </p>
+              </div>
+
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
+                <Link
+                  to={`/jobs/edit/${job.id}`}
+                  className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Edit Job
+                </Link>
+                <button
+                  onClick={() => onDelete(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Delete Job
+                </button>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export { JobPage as default, jobLoader };
